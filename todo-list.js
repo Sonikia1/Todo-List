acdba0ee17395eddfc01e955dfad2ec13d9db3f0
Nicholas Hughes <ndon89@yahoo.com>
2025-06-28 18:21:17 -0500

    Map data persists in localStorage. Id number doesn't decrease as it was causing problems. Correctly creates rows after page is reloaded. Have to put correct data in each row from the map.

--- todo-list.js
+++ todo-list.js
@@ -7,7 +7,29 @@ let submitBtns = null
 let todoArray = []
 let counter = 0
 
+//localStorage.clear()
+let todoMap = JSON.parse(localStorage.getItem("list"))
+todoMap = new Map(todoMap)
 
+console.log(todoMap)
+console.log(todoMap.size)
+
+
+
+
+if (todoMap.size == null) {
+  todoMap = new Map()
+  //localStorage.getItem("list", JSON.stringify(Array.from(todoMap.entries())))
+} else {
+  const rows = todoMap.size / 2
+  console.log(rows)
+  for (let i = 1; i <= rows; i++) {
+    addTemplate()
+    //textInput.textContent = todoMap.get(`item${i}`)
+    //dateInput.textContent = todoMap.get(`dateItem${i}`) //textInput and date are not defined. may have to make separate function
+  }
+
+}
 
 addBtn.addEventListener("click", function() {
   counter += 1
@@ -17,16 +39,63 @@ addBtn.addEventListener("click", function() {
     Btn.addEventListener("click", function() {
       let lastChar = Btn.id[Btn.id.length - 1]
       let itemToRemove = document.getElementById(`item${lastChar}`)
-      itemToRemove.remove()
-      counter -= 1
+      console.log(lastChar)
+      console.log(Btn.id)
+      if (Btn.id === `delete${lastChar}`) {
+        itemToRemove.remove()
+
+        localStorage.removeItem(`item${lastChar}`)
+        localStorage.removeItem(`dateItem${lastChar}`)
+
+        todoMap.delete(`item${lastChar}`)
+        todoMap.delete(`dateItem${lastChar}`)
+
+        //decreaseID(lastChar)
+
+        localStorage.setItem("list", JSON.stringify(Array.from(todoMap.entries())))
+        todoMap = new Map(JSON.parse(localStorage.getItem("list")))
+        
+      }
+      
+      
+
+      
+
+      //decrease id of buttons after delete
+      
     })
+    
   })
   submitBtns = document.querySelectorAll(".submit")
   submitBtns.forEach(function(Btn) {
     Btn.addEventListener("click", function() {
       let lastChar = Btn.id[Btn.id.length - 1]
       let itemToSave = document.getElementById(`item${lastChar}`)
-      console.log(itemToSave.querySelectorAll(".child")[0].value)
+      todoMap.set(`item${lastChar}`, itemToSave.querySelectorAll(".child")[0].value)
+      todoMap.set(`dateItem${lastChar}`, itemToSave.querySelectorAll(".child")[1].value)
+      localStorage.setItem("list", JSON.stringify(Array.from(todoMap.entries())))
+
+      todoMap = new Map(JSON.parse(localStorage.getItem("list")))
+      console.log("localStorage:", todoMap)
+
+      if (todoMap.get(`dateItem${lastChar}`).length == 0) { 
+        console.log("No Date")
+      }
+      else {
+        console.log(todoMap.get(`dateItem${lastChar}`))
+      }
+
+      if (todoMap.get(`item${lastChar}`).length == 0) { 
+        console.log("No ToDo")
+      }
+      else {
+        console.log(todoMap.get(`item${lastChar}`))
+      }
+      console.log(todoMap)
+      
+      
+      //console.log((itemToSave.querySelectorAll(".child")[1].value))
+      
       
     })
   })
@@ -70,6 +139,8 @@ function addTemplate() {
    //checker(submitBtn)
 
 
+   if (todoMap.size != null)
+
 }
 
 
@@ -82,13 +153,28 @@ function checker(tag) {
 }
 
 
-function addToList(content) {
-  todoArray.push(content)
-  const itemList = document.getElementsByClassName("item-list")[0]
-  let newDiv = document.createElement("div")
-  newDiv.textContent = content
-  itemList.appendChild(newDiv)
-  console.log(`Inside addToList: ${content}`)
+function decreaseID(id) {
+  items = document.querySelectorAll(".item")
+  items.forEach( function(item) {
+    let lastChar = item.id[item.id.length - 1]
+    try {
+      if (lastChar < id) {
+        let replace = lastChar - 1
+        item.id = `item${replace}`
+        const children = item.children
+        let delBtnChange = document.getElementById(`delete${lastChar}`)
+        let subBtnChange = document.getElementById(`submit${lastChar}`)
+        
+        delBtnChange.id = `delete${replace}`
+        subBtnChange.id = `submit${replace}`
+      } else {
+          null
+      }
+    } catch (e) {
+      console.log("disregard")
+    }
+
+  })
 }
 
 
